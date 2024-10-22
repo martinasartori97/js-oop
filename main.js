d99c66d2a0c9daa2745791393785ae0879150f09
martinasartori97 <martinasartori9@gmail.com>
2024-10-22 14:53:15 +0200

    edit js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 console.log("hello");
 
-class veicolo 
+
+/*In questo esercizio, dovrai creare una classe in JavaScript chiamata "Veicolo" con alcune proprietà e metodi.
+//La classe "Veicolo" dovrebbe avere le seguenti caratteristiche:
+ - marca
+ - anno
+ - colore
+ - porte
+ - carburante
+*/
+
+
+class veicolo {
+    constructor()
+
+}
